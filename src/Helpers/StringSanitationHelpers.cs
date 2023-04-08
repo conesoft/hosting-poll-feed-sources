22babdd5f9c0ac67d3a002445c8ca24d5c813834
David Spörri <davepermen@hotmail.com>
2023-04-08 17:30:51 +0200

    Projektdateien hinzufügen.

---
+++ src/Helpers/StringSanitationHelpers.cs
@@ -0,0 +1,31 @@
+﻿using System.Web;
+
+namespace Helpers;
+
+static public class StringSanitationHelpers
+{
+    public static string SafeFilename(this string filename, char replacement = '-')
+    {
+        return string.Join(replacement, filename.Split(Path.GetInvalidFileNameChars(), StringSplitOptions.RemoveEmptyEntries)).Replace('.', replacement);
+    }
+
+    public static string CleanUrl(this string url)
+    {
+        var split = url.Split('?');
+        if (split.Length > 1)
+        {
+            var queries = HttpUtility.ParseQueryString(split[1]);
+            queries.Remove("rss");
+            queries.Remove("utm_medium");
+            queries.Remove("utm_source");
+            queries.Remove("utm_campaign");
+            queries.Remove("utm_content");
+
+            if(queries.Count > 0)
+            {
+                return split[0] + "?" + queries;
+            }
+        }
+        return split[0];
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,60 @@
+﻿using CodeHollow.FeedReader;
+using Conesoft.Files;
+using Helpers;
+
+var timer = new PeriodicTimer(TimeSpan.FromMinutes(15));
+
+var settings = Conesoft.Hosting.Host.LocalSettings;
+var storage = Conesoft.Hosting.Host.GlobalStorage / "FromSources" / "Feeds";
+var feedstorage = storage / "Feeds";
+var entrystorage = storage / "Entries";
+
+var feeds = await settings.ReadFromJson<Feed[]>() ?? Array.Empty<Feed>();
+
+do
+{
+    await Task.WhenAll(feeds.Select(async f =>
+    {
+        try
+        {
+            var client = new HttpClient();
+
+            var file = feedstorage / Filename.From(f.Filename, "xml");
+
+            var xml = await client.GetStringAsync(f.Feedurl);
+            await file.WriteText(xml);
+
+            var feed = FeedReader.ReadFromString(xml);
+            var entries = feed.Items;
+
+            await Task.WhenAll(entries.Select(async e =>
+            {
+                var link = e.Link.StartsWith("http") ? e.Link : feed.Link + e.Link;
+
+                var entry = new Entry(
+                    Name: e.Title,
+                    Url: link,
+                    Description: e.Description,
+                    Category: f.Category
+                );
+
+                var entryfile = entrystorage / Filename.From(entry.Filename, "json");
+                await entryfile.WriteAsJson(entry);
+            }));
+        }
+        catch (Exception)
+        {
+        }
+    }));
+}
+while (await timer.WaitForNextTickAsync());
+
+record Feed(string Name, string Siteurl, string Feedurl, string Category)
+{
+    public string Filename => Name.SafeFilename();
+}
+
+record Entry(string Name, string Url, string Description, string Category)
+{
+    public string Filename => Url.CleanUrl().SafeFilename();
+}
