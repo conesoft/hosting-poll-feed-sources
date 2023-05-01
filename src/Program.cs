c50826e8e7bb8660ea13f5e68c0afb5dd41b041e
David Spörri <davepermen@hotmail.com>
2023-05-01 22:47:44 +0200

    fixing regex

--- src/Program.cs
+++ src/Program.cs
@@ -2,7 +2,13 @@
 using CodeHollow.FeedReader.Feeds;
 using Conesoft.Files;
 using Helpers;
+using Microsoft.AspNetCore.Http.Extensions;
 using System.Text.RegularExpressions;
+using System.Web;
+using System.Linq;
+
+var configuration = new ConfigurationBuilder().AddJsonFile(Conesoft.Hosting.Host.GlobalSettings.Path).Build();
+var conesoftSecret = configuration["conesoft:secret"] ?? throw new Exception("Conesoft Secret not found in Configuration");
 
 var timer = new PeriodicTimer(TimeSpan.FromMinutes(15));
 
@@ -40,17 +46,23 @@ do
                 link ??= e.Link.StartsWith("http") ? e.Link : feed.Link + e.Link;
 
                 var entry = new Entry(
-                    Name: e.Title,
+                    Name: HttpUtility.UrlDecode(e.Title),
                     Url: link,
                     Published: e.PublishingDate ?? DateTime.MinValue,
                     Description: e.Description,
-                    Category: f.Category
+                    Category: f.Category,
+                    Feed: f.Siteurl.CleanUrl().Replace("https://", "").Replace("http://", "").Replace("/", "").Replace("www.", "")
                 );
 
                 var entryfile = entrystorage / Filename.From(entry.Filename, "json");
+                var newentry = entryfile.Exists == false;
                 await entryfile.WriteAsJson(entry);
 
-                await SaveImage(link, entrystorage, entry.Filename);
+                var image = await SaveImage(link, entrystorage, entry.Filename);
+                if (newentry)
+                {
+                    await Notify(entry, image);
+                }
             }));
         }
         catch (Exception)
@@ -60,68 +72,53 @@ do
 }
 while (await timer.WaitForNextTickAsync());
 
-static async Task SaveImage(string link, Conesoft.Files.Directory storage, string filename)
+async Task Notify(Entry entry, Conesoft.Files.File? image)
+{
+    var title = entry.Name;
+    var message = $"from: {entry.Feed}";
+    var url = entry.Url;
+    var imageUrl = image != null ? $"https://kontrol.conesoft.net/content/feeds/thumbnail/{image.Name}" : "";
+
+    var query = new QueryBuilder
+    {
+        { "token", conesoftSecret },
+        { "title", title },
+        { "message", message },
+        { "url", url }
+    };
+    if (image != null)
+    {
+        query.Add("imageUrl", imageUrl);
+    }
+
+    await new HttpClient().GetAsync($@"https://conesoft.net/notify" + query.ToQueryString());
+}
+
+static async Task<Conesoft.Files.File?> SaveImage(string link, Conesoft.Files.Directory storage, string filename)
 {
     var client = new HttpClient();
 
     var html = await client.GetStringAsync(link);
 
+    var match = FindOgImageContentOrFirstImage().Match(html);
+
+    var image = match.Success ? match.Groups["imageurl"].Value.UrlWithoutQueryString() : null;
+
+    if (image != null)
     {
-        var match = FindOgImageContent().Match(html);
+        var extension = Path.GetExtension(image);
+        extension = string.IsNullOrEmpty(extension) ? "jpg" : extension;
 
-        if (match.Success && match.Groups.Count > 1)
-        {
-            var image = match.Groups[1].Value.UrlWithoutQueryString();
-            var bytes = await client.GetByteArrayAsync(image);
+        var file = storage / Filename.From(filename, extension);
 
-            var extension = Path.GetExtension(image);
-            extension = string.IsNullOrEmpty(extension) ? "jpg" : extension;
+        using var stream = await client.GetStreamAsync(image);
+        using var filestream = System.IO.File.OpenWrite(file.Path);
+        await stream.CopyToAsync(filestream);
 
-            var file = storage / Filename.From(filename, extension);
-
-            await file.WriteBytes(bytes);
-
-            return;
-        }
+        return file;
     }
 
-    {
-        var match = FindContentOgImage().Match(html);
-
-        if (match.Success && match.Groups.Count > 1)
-        {
-            var image = match.Groups[1].Value.UrlWithoutQueryString();
-            var bytes = await client.GetByteArrayAsync(image);
-
-            var extension = Path.GetExtension(image);
-            extension = string.IsNullOrEmpty(extension) ? "jpg" : extension;
-
-            var file = storage / Filename.From(filename, extension);
-
-            await file.WriteBytes(bytes);
-
-            return;
-        }
-    }
-
-    {
-        var match = FindFirstImage().Match(html);
-
-        if (match.Success && match.Groups.Count > 1)
-        {
-            var image = match.Groups[1].Value.UrlWithoutQueryString();
-            var bytes = await client.GetByteArrayAsync(image);
-
-            var extension = Path.GetExtension(image);
-            extension = string.IsNullOrEmpty(extension) ? "jpg" : extension;
-
-            var file = storage / Filename.From(filename, extension);
-
-            await file.WriteBytes(bytes);
-
-            return;
-        }
-    }
+    return null;
 }
 
 record Feed(string Name, string Siteurl, string Feedurl, string Category)
@@ -129,7 +126,7 @@ record Feed(string Name, string Siteurl, string Feedurl, string Category)
     public string Filename => Name.SafeFilename();
 }
 
-record Entry(string Name, string Url, DateTime Published, string Description, string Category)
+record Entry(string Name, string Url, DateTime Published, string Description, string Category, string Feed)
 {
     public string Filename => Url.CleanUrl().SafeFilename();
 }
@@ -145,4 +142,12 @@ partial class Program
 
     [GeneratedRegex("img.*?src=[\"\"'](.*?)[\"\"']", RegexOptions.IgnorePatternWhitespace | RegexOptions.IgnoreCase)]
     private static partial Regex FindFirstImage();
+
+    [GeneratedRegex(
+        "property=[\"']og:image[\"'] content=[\"'](?<imageurl>.+?)[\"']|" +
+        "content=[\"'](?<imageurl>.+?)[\"'] property=[\"']og:image[\"']|" +
+        "img(?!>)src=[\"'](?<imageurl>.*?)[\"']",
+        RegexOptions.IgnoreCase
+    )]
+    private static partial Regex FindOgImageContentOrFirstImage();
 }
